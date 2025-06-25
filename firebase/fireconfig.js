c344e0a643f32d5e5e2741d0e96ba9ab54bce3d5
SakshamSwarup <sakshamswarup2430@gmail.com>
2025-06-25 18:32:35 +0530

    feat: update Firebase configuration to use environment variables

--- firebase/fireconfig.js
+++ firebase/fireconfig.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import { env } from 'node:process';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyB7EdL79hRx6ND1bASiqKZ3Ib740wGKRdY',
-  authDomain: 'ecommerce-7b8bc.firebaseapp.com',
-  projectId: 'ecommerce-7b8bc',
-  storageBucket: 'ecommerce-7b8bc.firebasestorage.app',
-  messagingSenderId: '850063276078',
-  appId: '1:850063276078:web:7555bbe366356ca9bf47bf',
-  measurementId: 'G-LSBY2K08KN',
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
+  measurementId: env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
